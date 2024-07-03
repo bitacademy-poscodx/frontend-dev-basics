e9200cb0c159153b0de0d3994d8c7b939d07f038
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-07-03 09:55:27 +0900

    js basics #2

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -42,10 +42,30 @@ var o = {};
 var a = [];
 var n = null;
 
-console.log("i2:" + typeof(i2));
-console.log("s2:" + typeof(s2));
-console.log("b2:" + typeof(b2));
-console.log("o:" + typeof(o));
-console.log("a:" + typeof(a));
+console.log("i2:" + typeof(i2) + ":" + (i2 instanceof(Number)));
+console.log("s2:" + typeof(s2) + ":" + (s2 instanceof(String)));
+console.log("b2:" + typeof(b2) + ":" + (b2 instanceof(Boolean)));
+console.log("o:" + typeof(o) + ":" + (o instanceof(Object)));
+console.log("a:" + typeof(a) + ":" + (a instanceof(Array)));
 console.log("n:" + typeof(n));
 
+console.log("=== 객체타입(function) ======");
+function f1(a, b) {
+    return a + b;
+}
+
+var f2 = function(a, b) {
+    return a + b;
+}
+
+var f3 = new Function('a', 'b', 'return a+b;');
+
+console.log(f1(10, 20), f2(10, 20), f3(10, 20));
+console.log("f1:" + typeof(f1));
+console.log("f2:" + typeof(f2));
+console.log("f3:" + typeof(f3));
+
+
+console.log("=== Primitive Type도 메소드를 호출할 수 있다.(유사객체) ===");
+console.log(b2.valueOf());
+console.log(b.valueOf());    //console.log(new Boolean(b).valueof());
---
+++ javascript-practices/basics/ex03.js
@@ -0,0 +1,7 @@
+/* null과 undefined */
+
+var myVar1 = undefined; // 명시적으로 undefined 대입
+var myVar2;             // 암시적으로 undefined 대입
+var myVar3 = null;
+
+console.log(myVar1 + ":" + myVar2 + ":" + myVar3);
