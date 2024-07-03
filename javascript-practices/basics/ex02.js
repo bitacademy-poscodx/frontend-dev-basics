17b08c7c10fcbb2ab5b61f9fa5c3522eadc1eddf
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-07-03 11:08:30 +0900

    js basics #3

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -24,7 +24,7 @@
 
  */
 console.log("=== 기본타입(undefined, number, string, boolean) ======");
-var u;
+var u;   // var u = undefined; 과 동일, 자바스크립트는 선언과 정의가 구분없다.
 var i = 10;
 var s = 'hello world';
 var b = true;
